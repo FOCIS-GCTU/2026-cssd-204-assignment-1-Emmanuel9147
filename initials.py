7eb00c8f1cd78bda745b60bbb6ac1c6b2ae45c6f
Emmanuel9147 <2425400974@live.gctu.edu.gh>
2026-05-31 17:26:21 +0000

    Implement initials printing for EKO
    Added a program to print initials EKO in large letters.

--- initials.py
+++ initials.py
@@ -1,6 +1,34 @@
+# File: initials.py
+# Description: Print out my initials EKO in large letters.
+# Assignment Number: 2
+#
+# Name: Emmanuel Kwakye Osei
+# STUDENT ID: 2425400974
+# Email: 2425400974@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Emmanuel Kwakye Osei , this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
   pass #code goes here
+  
+    print("...EKO")
+
+    print()
+
+    print("...EEEEEEEEEEEE...KKK......KKK...OOOOOOOOOOOO")
+    print("...EEEEEEEEEEEE...KKK.....KKK....OOOOOOOOOOOO")
+    print("...EEE............KKK....KKK.....OOO......OOO")
+    print("...EEE............KKK...KKK......OOO......OOO")
+    print("...EEEEEEEEEEEE...KKKKKKK........OOO......OOO")
+    print("...EEEEEEEEEEEE...KKKKKK.........OOO......OOO")
+    print("...EEE............KKK..KKK.......OOO......OOO")
+    print("...EEE............KKK...KKK......OOO......OOO")
+    print("...EEEEEEEEEEEE...KKK....KKK.....OOOOOOOOOOOO")
+    print("...EEEEEEEEEEEE...KKK.....KKK....OOOOOOOOOOOO")
+
+    print()
 
 
 
